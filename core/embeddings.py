6065700e3292a1beae28d0c4be5fecd11b1bf5ce
Dmitriy Ushakov Air <udv8@ya.ru>
2026-01-29 23:25:17 +0300

    add LLM

--- core/embeddings.py
+++ core/embeddings.py
@@ -1,9 +1,14 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 
-def get_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name="BAAI/bge-large-en-v1.5",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True},
-    )
+def get_embeddings(cfg):
+    provider = cfg["embeddings"]["provider"]
+
+    if provider == "huggingface":
+        return HuggingFaceEmbeddings(model_name=cfg["embeddings"]["model"])
+
+    if provider == "openai":
+        return OpenAIEmbeddings()
+
+    raise ValueError("Unknown embeddings provider")
